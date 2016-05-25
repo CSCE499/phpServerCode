6be8a652bd40813ad0529e7fded929c6ac4ee115
Hoang, Phu <Jeankevi@users.noreply.github.com>
2016-05-25 10:04:59 -0700

    Add files via upload

--- DB_Functions.php
+++ DB_Functions.php
@@ -87,7 +87,7 @@ class DB_Functions{
 	//Get user schedule information/
 	public function getSingleSchedule($username,$value){
 		$data = json_decode($value,true);
-		$id = $data['id'];
+		$id = $data['event_num'];
 		
 		$result = mysql_query("SELECT * FROM event WHERE ev_uname LIKE '$username' and event_num = '$id'") or die(mysql_error());
 		$numRows = mysql_num_rows($result);
@@ -101,22 +101,30 @@ class DB_Functions{
 	//Insert user schedule information
 	public function insertSchedule($username,$value){
 		$data = json_decode($value,true);
-		$id = $data['id'];
-		$title = $data['title'];
+		$id = $data['event_num'];
+		$title = $data['event_title'];
 		$location = $data['location'];
-		$s_date = $data['startTime'];
-		$e_date = $data['endTime'];
-		$done_date = $data['doneDate'];
-		$allDay = $data['allDay'];
-		$note = $data['note'];	
+		$s_date = $data['s_date'];
+		$e_date = $data['e_date'];
+		$done_date = $data['done_date'];
+		$allDay = $data['all_day'];
+		$note = $data['notes'];	
 		$priority = $data['priority'];
 		$repeat = $data['days'];
 		$color = $data['color'];
 		$course = $data['course'];
+		if(array_key_exists('super_event_id',$data)){
+			$super_event_id = $data['super_event_id'];
+			$sql = "INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course,super_event_id) VALUES ('$username','$id','$title','$location','$s_date','$e_date','$done_date','$allDay','$note','$priority','$repeat','$color','$course','$super_event_id')";
+		}else{
+			$sql = "INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course) VALUES ('$username','$id','$title','$location','$s_date','$e_date','$done_date','$allDay','$note','$priority','$repeat','$color','$course')";
+		}
 		
+		//echo $repeat,'repeat'."<br/>";
+		//$result = mysql_query("INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course) VALUES ('$username','$id','$title','$location','$s_date','$e_date','$done_date','$allDay','$note','$priority','$repeat','$color','$course') ");
 		if($repeat == null){
 			//echo empty($repeat),"<br/>";
-			$result = mysql_query("INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course) VALUES ('$username','$id','$title','$location','$s_date','$e_date','$done_date','$allDay','$note','$priority','$repeat','$color','$course')") or die('Invalid query: ' . mysql_error());
+			$result = mysql_query($sql) or die('Invalid query: ' . mysql_error());
 		}else{
 			
 			$sDate = new DateTime($s_date);
@@ -127,16 +135,16 @@ class DB_Functions{
 			}else{				
 				$diff = (int)date_diff($sDate,$dDate)->format('%a');
 			}	
-			echo $diff, "<br />";
-			$result = mysql_query("INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course) VALUES ('$username','$id','$title','$location','$s_date','$e_date','$done_date','$allDay','$note','$priority','$repeat','$color','$course')") or die('Invalid query: ' . mysql_error());
+			//echo $diff, "<br />";
+			$result = mysql_query($sql) or die('Invalid query: ' . mysql_error());
 			
 			for($i = 0; $i < $diff;$i++){
 				$sDate = $sDate->add(DateInterval::createFromDateString('1 day'));
 				$eDate = $eDate->add(DateInterval::createFromDateString('1 day'));
-				echo $sDate->format('Y-m-d H:i:s'), "<br />";
-				echo $eDate->format('Y-m-d H:i:s'), "<br />";
-				echo $sDate->format('D'), "<br />";
-				echo "increment of $i", "<br />";
+				//echo $sDate->format('Y-m-d H:i:s'), "<br />";
+				//echo $eDate->format('Y-m-d H:i:s'), "<br />";
+				//echo $sDate->format('D'), "<br />";
+				//echo "increment of $i", "<br />";
 				switch($sDate->format('D')){
 					case 'Mon':
 						$match = 'M';
@@ -161,19 +169,19 @@ class DB_Functions{
 						break;
 				}
 				
-				echo $match, "<br />";
-				echo$repeat, "<br />";
+				//echo $match, "<br />";
+				//echo$repeat, "<br />";
 				if(strpos($repeat,$match) !== false){
-					echo strpos($repeat,$match), "<br />";
-					echo "MATCH_STRING", "<br />";
+					//echo strpos($repeat,$match), "<br />";
+					//echo "MATCH_STRING", "<br />";
 					$newEDate = $eDate->format('Y-m-d H:i:s');
 					$newSDate = $sDate->format('Y-m-d H:i:s');
 					$newId = rand();
 					$result = mysql_query("INSERT INTO event (ev_uname,event_num,event_title,location,s_date,e_date,done_date,all_day,notes,priority,days,color,course,super_event_id) VALUES ('$username','$newId','$title','$location','$newSDate','$newEDate','$done_date','$allDay','$note','$priority','$repeat','$color','$course','$id')") or die('Invalid query: ' . mysql_error());
 					if($result){
-						echo "SUCCESS", "<br />";
+						//echo "SUCCESS", "<br />";
 					}else{
-						echo "FAIL TO INSERT", "<br />";
+						//echo "FAIL TO INSERT", "<br />";
 					}
 				}
 			}			
@@ -189,23 +197,30 @@ class DB_Functions{
 		}else{
 			return false;
 		}
+		
+		
 	}
 	
 	//Update user schedule information
 	public function updateSchedule($username,$value){
 		$data = json_decode($value,true);
-		$id = $data['id'];
-		$title = $data['title'];
+		$id = $data['event_num'];
+		$title = $data['event_title'];
 		$location = $data['location'];
-		$s_date = $data['startTime'];
-		$e_date = $data['endTime'];
-		$done_date = $data['doneDate'];
-		$allDay = $data['allDay'];
-		$note = $data['note'];	
+		$s_date = $data['s_date'];
+		$e_date = $data['e_date'];
+		$done_date = $data['done_date'];
+		$allDay = $data['all_day'];
+		$note = $data['notes'];	
 		$priority = $data['priority'];
 		$repeat = $data['days'];
 		$color = $data['color'];
 		$course = $data['course'];
+		if(array_key_exists('super_event_id',$data)){
+			$super_event_id = $data['super_event_id'];
+		}else{
+			$super_event_id = null;
+		}
 		
 		$result = mysql_query("UPDATE event SET event_title = '$title',location = '$location',s_date = '$s_date',e_date = '$e_date',done_date = '$done_date',all_day = '$allDay',notes = '$note',priority = '$priority',days = '$repeat',color='$color',course='$course' WHERE ev_uname = '$username' and event_num = '$id' ");
 		
@@ -223,38 +238,137 @@ class DB_Functions{
 	
 	//Update user schedule information
 	public function deleteSchedule($username,$id){
-		//$data = json_decode($value,true);
-		//$id = $data['id'];
 		
 		$result = mysql_query("DELETE FROM event WHERE ev_uname = '$username' and event_num = '$id' ");
-		//$check = mysql_query("SELECT * FROM event WHERE ev_uname = '$username' and event_num = '$id'");
-		//$numRows = mysql_num_rows($check);
-		if($result){
-			return true;	//delete successfully.
-		}else{
+		$check = mysql_query("SELECT * FROM event WHERE ev_uname = '$username' and event_num = '$id'");
+		$numRows = mysql_num_rows($check);
+		if($numRows > 0){
 			return false;	//cannot delete.
+		}else{
+			return true;	//delete successfully.
 		}
 	}
 	
 	//Update user schedule information
 	public function computeSchedule($username,$time){
-		
+		$timeBlock = new DateTime($time);
+		$timeEndOfDay = new DateTime('23:00');
+		$numRows = 0;
+		$result =  mysql_query("SELECT * FROM event WHERE ev_uname = '$username' and course = 1 and super_event_id is null ")or die('Invalid query1: ' . mysql_error());
+		//print_r($result);//test
+		if(mysql_num_rows($result)>0){
+
+			//Check if there is old study schedule
+			$check = $check = mysql_query("SELECT * FROM event WHERE ev_uname = '$username' and course = 0 and super_event_id in (SELECT event_num FROM event where ev_uname = '$username' and course = 1 and super_event_id is null) ")or die('Invalid query2: ' . mysql_error());
+			$numRows = mysql_num_rows($check);
+			
+			if(mysql_num_rows($check) > 0){
+				
+				//Delete all the old study event and create new schedule
+				$oldStudySchedule = mysql_query("DELETE FROM event WHERE ev_uname = '$username' and course = 0 and super_event_id in (SELECT event_num FROM (SELECT event_num FROM event where ev_uname = '$username' and course = 1 and super_event_id is null) as id) ")or die('Invalid query3: ' . mysql_error());
+				if(mysql_affected_rows()>0){	//If successful delete, then update new study schedule
+					while($row = mysql_fetch_assoc($result)){
+						$newEvent = $this->computeTime($row,$timeBlock);
+						$this->insertSchedule($username,$newEvent);
+					}
+					return true;
+				}else{
+					return false; //cannot update new study schedule
+				}
+				
+			}else{	//If there is none any old schedule, create a new one
+				echo "ready  to insert2","<br/>";
+				while($row = mysql_fetch_assoc($result)){
+					$newEvent = $this->computeTime($row,$timeBlock);
+					$this->insertSchedule($username,$newEvent);
+				}
+				return true;
+			}	
+			
+		}else{
+			return false;
+		}
 	}
-	
 	//Compute the study Time
-	public function computeTime($result){
-		//$name = 'admin';
-		//$id = '3388240525438111101';
-		//$result =  mysql_query("SELECT * FROM event WHERE ev_uname = '$name' and event_num = '$id'")or die('Invalid query: ' . mysql_error());
-		//$result =  mysql_query("SELECT * FROM event WHERE ev_uname = '$name' and course = 1 and super_event_id is null ")or die('Invalid query: ' . mysql_error());
-		$event = @mysql_fetch_array($result);
+	public function computeTime1($event,$timeBlock){
+		$copy = $timeBlock;
+		while($row = mysql_fetch_assoc($event)){
+			$next = mysql_fetch_assoc($event);
+			while(new DateTime($row['s_date'])->format('Y-m-d') === new DateTime($next['s_date'])->format('Y-m-d')){
+				if($row['priority'] > $next['priority']){					
+					$this->computeTime($row,$copy);
+					$copy = $copy->setTime(new DateTime($row['s_date'])->format('H')
+				}
+			}
+			switch($event['event_num']){
+				
+			}
+			$newEvent = $this->computeTime($row,$timeBlock);
+		}
+		$username = $event['ev_uname'];
+		$event['super_event_id'] = $event['event_num'];
 		
 		$eDate = new DateTime($event['e_date']);
 		$sDate = new DateTime($event['s_date']);
 		$diff = date_diff($eDate,$sDate);
 		$duration = ($diff->h+$diff->i/60);
 		$newTime = round($duration*2);
-		return $newTime;
+		
+		/** If there is empty slot after $timeBlock. */
+		
+		//Generat new study event.		
+		$newTitle = 'Study for '.$event['event_title'];
+		$newId = rand();
+		$newSDate = $sDate->setTime($timeBlock->format('H'),$timeBlock->format('i'))->format('Y-m-d H:i:s');
+		$newEDate = $eDate->setTime($timeBlock->format('H')+$newTime,$timeBlock->format('i'))->format('Y-m-d H:i:s');
+		
+		$event['event_title'] = $newTitle;
+		$event['event_num'] = $newId;
+		$event['s_date'] = $newSDate;
+		$event['e_date'] = $newEDate;
+		$event['location'] = '';
+		$event['course'] = 0;
+		
+		
+		print_r($event);
+		$newEvent = json_encode($event);
+		//echo $newEvent;
+		
+		return $newEvent;
+	}
+	
+	//Compute the study Time
+	public function computeTime($event,$timeBlock){
+		$username = $event['ev_uname'];
+		$event['super_event_id'] = $event['event_num'];
+		
+		$eDate = new DateTime($event['e_date']);
+		$sDate = new DateTime($event['s_date']);
+		$diff = date_diff($eDate,$sDate);
+		$duration = ($diff->h+$diff->i/60);
+		$newTime = round($duration*2);
+		
+		/** If there is empty slot after $timeBlock. */
+		
+		//Generat new study event.		
+		$newTitle = 'Study for '.$event['event_title'];
+		$newId = rand();
+		$newSDate = $sDate->setTime($timeBlock->format('H'),$timeBlock->format('i'))->format('Y-m-d H:i:s');
+		$newEDate = $eDate->setTime($timeBlock->format('H')+$newTime,$timeBlock->format('i'))->format('Y-m-d H:i:s');
+		
+		$event['event_title'] = $newTitle;
+		$event['event_num'] = $newId;
+		$event['s_date'] = $newSDate;
+		$event['e_date'] = $newEDate;
+		$event['location'] = '';
+		$event['course'] = 0;
+		
+		
+		print_r($event);
+		$newEvent = json_encode($event);
+		//echo $newEvent;
+		
+		return $newEvent;
 	}
 	
 	//Encrupting password. Returns salt and encrypted password.
